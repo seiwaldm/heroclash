f9792f587dd17af327809e170957c631e0acba66
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2021-04-18 17:52:05 +0200

    user-stories update

--- heroclash.js
+++ heroclash.js
@@ -63,20 +63,20 @@ class Heroclash {
     const result = stats1[discipline] - stats2[discipline];
 
     // PLAYING AROUND WITH LOCAL STRAPI INSTALLATION:
-    // const response = fetch("http://localhost:1337/clashes", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   },
-    //   body: `{
-    //   "hero1": ${p1.deck[0].id},
-    //   "hero2": ${p2.deck[0].id},
-    //   "discipline": "${discipline}",
-    //   "winner": 1,
-    //   "margin": ${Math.abs(result)}
-    //   }
-    //   `
-    // });
+    const strapiPost = fetch("http://localhost:1337/battles", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: `{
+      "hero1": ${p1.deck[0].id},
+      "hero2": ${p2.deck[0].id},
+      "discipline": "${discipline}",
+      "winner": 1,
+      "margin": ${Math.abs(result)}
+      }
+      `
+    });
     //TODO: refactor with result from determineWinner:
     if (result > 0) {
       p1.deck.push(this.players[0].deck.shift());
--- user-stories.md
+++ user-stories.md
@@ -1,7 +1,8 @@
-- ich kann zwischen 1 (gegen ki) und 2 spieler (hot seat) wählen
-- ich kann die spielregeln nachlesen
-- ich kann die anzahl der karten für mein spiel auswählen
-- ich kann die ansicht auf einen mobilen modus (gegenüber sitzend) umschalten
-- ich kann spiele speichern/wiederaufnehmen
-- ich kann auf die ergebnisse alter spiele zugreifen (statistik)
-- ich kann online gegen freunde spielen
+[x] ich kann die anzahl der karten für mein spiel auswählen
+[x] ich kann zwischen 1 (gegen ki) und 2 spieler (hot seat) wählen
+[ ] ich kann die spielregeln nachlesen
+[ ] ich kann in einer gallerie alle verfügbaren helden ansehen
+[ ] ich kann die ansicht auf einen mobilen modus (gegenüber sitzend) umschalten
+[ ] ich kann spiele speichern/wiederaufnehmen
+[ ] ich kann auf die ergebnisse alter spiele zugreifen (statistik)
+[ ] ich kann online gegen freunde spiele
